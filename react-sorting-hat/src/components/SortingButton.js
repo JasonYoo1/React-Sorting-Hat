af737b5f6e1aaf4ce0b75d7bf911592ee641ee8c
JasonYoo1 <jyoojs@gmail.com>
2019-08-05 14:03:00 -0700

    not main project for Monday Aug 5

---
+++ react-sorting-hat/src/components/SortingButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+ const SortingButton = props =>{
+     return (
+         <div>
+            <input type="submit" className="btn btn-primary" value="Create Student" />
+
+         </div>
+     )
+ }
+
+ export default SortingButton;
